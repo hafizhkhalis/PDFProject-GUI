59fdea1cfc687be595733e8bfcd00806ad518997
Hakhajid <hafizh.khalis27@outlook.com>
2023-06-24 10:15:00 +0700

    Add project files.

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PDFProject_GUI
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.box_lembar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.box_filename = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(12, 59);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(133, 40);
+            this.btn_browse.TabIndex = 0;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(151, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(622, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_export
+            // 
+            this.btn_export.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn_export.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_export.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_export.Location = new System.Drawing.Point(712, 423);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(133, 40);
+            this.btn_export.TabIndex = 2;
+            this.btn_export.Text = "Export";
+            this.btn_export.UseVisualStyleBackColor = false;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(148, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(659, 284);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PDF Count Page App";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(782, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cek";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Jumlah Lembar";
+            // 
+            // box_lembar
+            // 
+            this.box_lembar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.box_lembar.Location = new System.Drawing.Point(12, 168);
+            this.box_lembar.Name = "box_lembar";
+            this.box_lembar.ReadOnly = true;
+            this.box_lembar.Size = new System.Drawing.Size(54, 27);
+            this.box_lembar.TabIndex = 7;
+            this.box_lembar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(35, 440);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Created by Hafizh Khalis";
+            // 
+            // box_filename
+            // 
+            this.box_filename.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.box_filename.Location = new System.Drawing.Point(529, 430);
+            this.box_filename.Name = "box_filename";
+            this.box_filename.Size = new System.Drawing.Size(166, 27);
+            this.box_filename.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(529, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nama File";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(812, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ver. 2.23";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(879, 486);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.box_filename);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.box_lembar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_browse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_browse;
+        private TextBox textBox1;
+        private Button btn_export;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private TextBox box_lembar;
+        private Label label3;
+        private TextBox box_filename;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Dashboard.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iText.Forms.Form.Element;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Annot;
+using iText.Kernel.Pdf.Filespec;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace PDFProject_GUI
+{
+    public partial class Dashboard : Form
+    {
+        int totalPdfPage = 0;
+        int no = 0;
+        DataTable dataTable = new DataTable();
+        string folderPath;
+
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            // Show the folder dialog and check if the user clicked OK
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected folder path
+                string folderPath = folderBrowserDialog.SelectedPath;
+
+                // Do something with the selected folder path
+                textBox1.Text = folderPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            totalPdfPage = 0;
+            totalPdfPage = 0;
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                dataTable = new DataTable();
+
+                dataTable.Columns.Add("Nama", typeof(string));
+                dataTable.Columns.Add("Tanggal Lahir", typeof(string));
+                dataTable.Columns.Add("No MR", typeof(string));
+                dataTable.Columns.Add("ID Badge", typeof(string));
+                dataTable.Columns.Add("Jumlah Lembar", typeof(int));
+
+                folderPath = textBox1.Text;
+                textBox1.Text = "";
+                string[] pdfFiles = Directory.GetFiles(folderPath, "*.pdf", SearchOption.AllDirectories);
+
+                foreach(string file in pdfFiles)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(file);
+                    PdfDocument pdfDoc = new PdfDocument(new PdfReader(file));
+                    int pageCount = pdfDoc.GetNumberOfPages();
+                    totalPdfPage += pageCount;
+
+                    if (fileName.Split("_").Length == 3)
+                    {
+                        dataTable.Rows.Add(fileName.Split("_")[0], fileName.Split("_")[1], fileName.Split("_")[2], "none", pageCount);
+                    }else if(fileName.Split("_").Length == 2){
+                        dataTable.Rows.Add(fileName.Split("_")[0], fileName.Split("_")[1], "none", "none", pageCount);
+                    }
+                    else 
+                    {
+                        dataTable.Rows.Add(fileName.Split("_")[0], fileName.Split("_")[1], fileName.Split("_")[2], fileName.Split("_")[3], pageCount);
+                    }
+
+                }
+
+                dataTable.Rows.Add("", "", "", "Total:", totalPdfPage);
+                box_lembar.Text = $"{totalPdfPage}";
+
+                // Set the DataTable as the data source for the DataGridView
+                dataGridView1.DataSource = dataTable;
+
+            }
+            else
+            {
+                MessageBox.Show("Silahkan Pilih Lokasi Penyimpanan File PDF");
+            }
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            { 
+                MessageBox.Show("Data yang di export belum ada");
+                return;
+            }
+            if (!string.IsNullOrEmpty(box_filename.Text))
+            {
+                using (ExcelPackage excelPackage = new ExcelPackage())
+                {
+                    // Create a new worksheet
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Master Data");
+
+                    // Load data from the DataTable to the worksheet
+                    worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
+
+                    // Mendapatkan kolom terakhir yang berisi data
+                    int lastColumn = worksheet.Dimension.End.Column;
+
+                    // Mengubah nilai di kolom terakhir pada baris terakhir
+                    int lastRow = worksheet.Dimension.End.Row;
+                    int lastRowMinOne = lastRow - 1;
+                    worksheet.Cells[lastRow, lastColumn].Formula = "SUM(E2:E" + lastRowMinOne + ")";
+
+                    // Mengatur gaya sel untuk setiap baris menjadi terpusat
+                    for (int row = 1; row <= lastRow; row++)
+                    {
+                        worksheet.Row(row).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    }
+
+                    // Make the header row bold
+                    ExcelRange headerRow = worksheet.Cells["A1:E1"];
+                    headerRow.Style.Font.Bold = true;
+
+
+                    // Apply borders to cells
+                    ExcelRange range = worksheet.Cells[worksheet.Dimension.Address];
+                    range.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    range.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                    range.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+
+                    // Menggabungkan sel-sel kolom
+                    worksheet.Cells[lastRow, 1, lastRow, 4].Merge = true;
+
+                    worksheet.Cells["A" + lastRow].Value = "Total:";
+                    worksheet.Cells["A" + lastRow].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                    worksheet.Row(lastRow).Style.Font.Bold = true;
+
+                    // Auto-fit Column
+                    worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                    string namaFile = box_filename.Text;
+
+                    // Save the Excel package in the current directory
+                    string filePath = Path.Combine(folderPath, namaFile + ".xlsx");
+                    excelPackage.SaveAs(new FileInfo(filePath));
+
+                    // Show a prompt box to open or not open the Excel file
+                    DialogResult result = MessageBox.Show("Apakah Mau Membuka File Excel?", "Open Excel File", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        // Open the Excel file using the default associated application
+                        Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
+                    }
+                    else
+                    {
+                        MessageBox.Show("File Excel disimpan pada: " + folderPath + "\\" + namaFile + ".xlsx");
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Masukkan Nama File Terlebih Dahulu");
+            }
+              
+        }
+    }
+}
